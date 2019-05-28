8fb6c7d4524a19ff7167d625eae4941d3f64f03d
Alejandro Cordero <a.cordero@students.chapelfield.org>
2019-05-28 16:53:17 -0400

    First try

---
+++ bob.py
@@ -0,0 +1,10 @@
+#Name a and k
+import time
+import random
+def displayIntro():
+    print ("you are in a house you have no idea how you got there")
+    print ("you see one door in every side of the room with 4 sides")
+    print ("there a box in the middle of the room")
+    print ()
+
+displayintro() 
