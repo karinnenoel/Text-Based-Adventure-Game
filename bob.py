38469d0a587ef05d43b160fe9f1a8a105a1845ca
Karetwins1 <43041672+Karetwins1@users.noreply.github.com>
2019-06-03 00:43:52 -0400

    Update bob.py

--- bob.py
+++ bob.py
@@ -1,4 +1,4 @@
-#Name a and k
+ #Name a and k
 import time
 import random
 def displayIntro():
@@ -6,8 +6,34 @@ def displayIntro():
     print ("you see one door in every side of the room with 4 sides")
     print ("there a box in the middle of the room")
     print ()
-def choosedoor ()
+def choosedoor()
 door = ""
-while door !+"1"
-displayIntro() 
+while door !="1" and door !="2" and door !="3" and door !="3" and door!="4" and door !="5" # input validation
+door = input ("which door will you choose?(1 or 2 or 3 or 4 or 5)
+return door
+def checkdoor(chosendoor)
 
+print("you go into the door")
+time.sleep(3)
+print ("you look inside the room")
+print()
+time.sleep()
+correctdoor == randint (1)
+door 2 == randint (2)
+door 3 == randint (3)
+door 4 == randint (4)
+door 5 == randint (5)
+
+if chosendoor == correctdoor:
+print("the room is blue")
+if chosendoor == door 2
+print("this room is green")
+if chosendoor == door 3
+print ("this room is black")
+if chosendoor == door 4
+print ("this room is not blue")
+if chosendoor == door 5
+print ("this room is a number") 
+displayIntro()
+              
+choosedoor()
