fefd65b144a6eef24204eb18f7d0192ce1eb9005
Karinne App <k.app@students.chapelfield.org>
2019-06-04 16:00:59 -0400

    Change the story for the north room.

--- bob.py
+++ bob.py
@@ -1,22 +1,23 @@
 def start():
-    print("You wake up in a dark, square room. In the middle of the room is a box, and there are four doors, one on each side of the room.")
-    print("You can go north, east, south, or west. You can also open the box.")
-    print("1. Go north")
-    print("2. Go east")
-    print("3. Go south")
-    print("4. Go west")
+    print("You wake up in a dark, square room. In the middle of the room there is a box. There are four doors and one is on each side of the room.")
+    print("You can go North, East, South, or West. You can also open the mysterious box.")
+    print("1. Go North")
+    print("2. Go East")
+    print("3. Go South")
+    print("4. Go West")
     print("5. Open the box")
     choice = input("What do you choose? ")
     start 
     def northRoom():
-        print("youer in a blue room with a powerful light coming in")
-        print("go back to door is 1")
-        print("go to light 2")
-        roomone = input ("what do you choose? ")
+        print("You enter the north room. There is an ethereal looking light beaming in the center of the floor.")
+        print("Will you go back to the door? Option - 1")
+        print("Or will you go to light? Option - 2")
+        roomone = input ("What do you choose? ")
         if roomone == "1":
             start()
         elif roomone == "2":
-            print ("you die and go back to start")
+            print ("You were burnt to a crisp lol. Would you like to respawn?")
+            print ("Yes? or No?") ## add way to optionally restart the game via user input yes = start no = kill program
             start()
         else:
             print ("stop that")
