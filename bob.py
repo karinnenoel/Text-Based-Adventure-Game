69466e46446069a0e3781230d2c535068cef6ad2
karinnenoel <43042385+karinnenoel@users.noreply.github.com>
2019-06-04 16:16:38 -0400

    Change the storyline of the East Room and remove extra code.

--- bob.py
+++ bob.py
@@ -10,7 +10,7 @@ def start():
     choice = input("What do you choose? ")
     start 
     def northRoom():
-        print("You enter the north room. There is an ethereal looking light beaming in the center of the floor.")
+        print("You enter the north room and the walls are covered in blue paint. There is an ethereal looking light beaming in the center of the floor.")
         print("Will you go back to the door? Option - 1")
         print("Or will you go to light? Option - 2")
         roomone = input ("What do you choose? ")
@@ -19,23 +19,11 @@ def start():
         elif roomone == "2":
             print ("You were burnt to a crisp lol. Would you like to respawn?")
             print ("Yes? or No?") ## add way to optionally restart the game via user input yes = start no = kill program
-=======
- def northRoom():
-        print("youer in a blue room with a powerful light coming in")
-        print("go back to door is 1")
-        print("go to light 2")
-        roomOne = input ("what do you choose? ")
-        if roomOne == "1":
-            start()
-        elif roomOne == "2":
-            print ("you die and go back to start")
->>>>>>> 38daf2cba1a1416efbe4d389fc9d5bce9a28e5f8
-            start()
         else:
-             print ("stop that")
+            print ("Stop that u lil turkey!")
     def eastRoom():
-        print("youer in yellow room")
-        print("you feel a bad felling and than you see it")
+        print("You walk into a yellow room")
+        print("A bad feeling starts to grow in the pit of your stomach. Than you see it!")
         print("A BIG SPIDER IS RIGHT INFRONT OF YOU")
         print("you run out 1")
         print("you give the spider a big hug 2")
